6e3f052a84e2b67a7bf84d0fc887baeb364bc3a4
Wowo10 <wojtekwojtek529@gmail.com>
2020-12-21 00:42:12 +0100

    Day5 - part two.

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,10 +1,10 @@
 mod functions;
 
 fn main() {
-    let input = functions::file_utils::get_input();
+    let mut input = functions::file_utils::get_input();
 
     let mut max_id = 0;
-    for pat in input {
+    for pat in &input {
         let id = pat.id();
         if id > max_id {
             max_id = id;
@@ -12,4 +12,19 @@ fn main() {
     }
 
     println!("Result: {}", max_id);
+
+    input.sort_by(|x, y| x.id().cmp(&y.id()));
+    
+    let mut prev = input.first().unwrap().id();
+    for pat in input.iter().skip(1) {
+        if pat.id() -1 == prev
+        {
+            prev = pat.id();
+        }
+        else{
+            break;
+        }
+    }
+
+    println!("My seat: {}", prev+1);
 }
